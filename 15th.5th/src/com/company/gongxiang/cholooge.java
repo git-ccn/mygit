74f4df0ec75b4f75027a3edad06489e8a73577ea
git-ccn <90503472+git-ccn@users.noreply.github.com>
2021-12-02 17:05:54 +0800

    Add files via upload

---
+++ 15th.5th/src/com/company/gongxiang/cholooge.java
@@ -0,0 +1,14 @@
+package com.company.gongxiang;/*
+ *开发人员                 ：陈诺
+ *开发人学号               ：2019110502
+ *开发时间                 ：
+ */
+
+public abstract class cholooge {
+    public abstract void method();
+    public void display() {
+        System.out.println("方法method()被调用");
+        System.out.println("调用时间2017-11-5 10：10：10");
+    }
+}
+
---
+++ 15th.5th/src/com/company/gongxiang/looge.java
@@ -0,0 +1,13 @@
+package com.company.gongxiang;/*
+ *开发人员                 ：陈诺
+ *开发人学号               ：2019110502
+ *开发时间                 ：
+ */
+
+public class looge extends cholooge {
+    @Override
+    public void method() {
+        System.out.println("调用成功");
+    }
+}
+
